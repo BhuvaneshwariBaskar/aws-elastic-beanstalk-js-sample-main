f844baa370e5c269b9618187d9347c5fa6565b1b
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-10-30 22:45:25 +0530

    changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 
 app.get("/", (req, res) =>
-  res.send("Hello All, Happy Learning --my application checking,Hello morning")
+  res.send("Hello All, Happy Learning --my application checking")
 );
 
 app.listen(port);
