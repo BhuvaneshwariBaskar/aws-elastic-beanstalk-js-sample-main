6f009310fa635d7cb3a512bb4f7afc7ab97cb865
BhuvaneshwariBaskar <bhuvaneshwarib.cse@citchennai.net>
2024-10-30 12:35:28 +0530

    Changes for checking

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello All, Happy Learning -- Updated Version'));
+app.get("/", (req, res) =>
+  res.send("Hello All, Happy Learning --my application checking")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
